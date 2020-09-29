b294abdebd844de4927be3a3166dc2493687fa1c
Andrew Pham <andrewpham92@gmail.com>
2020-09-29 16:34:40 -0400

    first init

---
+++ 2-medium/Read-And-Write.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("Input file: ", function (inputFile) {
+  fs.readFile(inputFile, function (error, buffer) {
+    if (error) {
+      console.log(error.message);
+      readline.close();
+      return;
+    }
+    readline.question("Output file: ", function (outputFile) {
+      readline.close();
+      
+      const content = buffer.toString();
+      const upperCased = content.toUpperCase();
+      // Create a new output file then insert the content to that file
+      fs.writeFile(outputFile, upperCased, function (error) {
+        if (error) {
+          console.log(error.message);
+          return;
+        }
+        console.log("Wrote to file ", outputFile);
+      });
+    });
+  });
+});
